30030e3aa1e1ba01cf2498221997b288a77d0b5b
llOrtegall <IvanOrtega_97@hotmail.com>
2026-01-05 12:13:23 -0500

    feat: delete all files to new project

--- src/app/new/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function NewAddPage() {
-  return (
-    <form>
-      <input type="text" placeholder="Title" />
-      <textarea placeholder="Description" />
-      <button type="submit">Add</button>
-    </form>
-  )
-}
