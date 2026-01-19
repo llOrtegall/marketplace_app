d9c9eac72faa26abf63594012339829336ffa8d6
llortegall <ivanortega_97@hotmail.com>
2026-01-19 11:56:22 -0500

    feat: implement authentication with NextAuth and Google provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
-export default function Home() {
+import { auth, signIn, signOut } from "@/auth";
+
+export default async function Home() {
+  const session = await auth();
+
   return (
     <section>
-    <h1>Welcome to My Next.js App</h1>
-    </section>  
+      <h1>Welcome to My Next.js App</h1>
+      {
+        session ? (
+          <form action={async () => {
+            "use server"
+            await signOut()
+          }}>
+
+            <p>You are logged in as {session?.user?.email}</p>
+            <button type="submit">Sign Out</button>
+          </form>
+        ) : (
+          <form
+            action={async () => {
+              "use server"
+              await signIn("google")
+            }}
+          >
+            <button type="submit">Signin with Google</button>
+          </form>
+        )
+      }
+    </section>
   );
 }
